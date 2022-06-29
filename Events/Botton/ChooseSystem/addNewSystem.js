c7d8b280b61628375ccbbc40ffc7d11b96bd21f7
whiteApple03 <108337603+whiteApple03@users.noreply.github.com>
2022-06-29 17:37:06 +0700

    fuck

---
+++ Events/Botton/ChooseSystem/addNewSystem.js
@@ -0,0 +1,41 @@
+function addNewSystem(botton) {
+
+
+
+
+    let system = document.createElement('tr');
+    system.id = `row${n}`;
+    
+    system.innerHTML = "<tr id=`row${n}`></tr>";
+    document.querySelector("#placeForNewSystem").append(system);
+    rowId = document.querySelector(`#row${n}`);
+
+
+    for (let i = 0; i < 6; i++) {
+        let contentOfSystem = ""
+        let now = new Date();
+        contentOfSystem = getNextTableElement(i, now);
+
+        rowId.insertAdjacentHTML('beforeend', contentOfSystem);
+    }
+
+    n += 1; 
+    let AlertAboutNoActiveSystem = document.querySelector("#noActiveSystem");
+
+    if (n > 0) {
+
+        AlertAboutNoActiveSystem.style.display = "none";        
+
+    }
+    if (n >= limitOfSystem) {
+        botton.style.display = "none";
+        
+        let limit = document.createElement('span');
+        limit.id = 'AlertAboutLimit';
+        limit.textContent = 'Достигнут лимит систем';
+        botton.after(limit);
+        
+        return 
+    } 
+
+}
--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,4 @@
-
+// import addNewSystem from "../Events/Botton/ChooseSystem/addNewSystem.js"
 
 const limitOfSystem = 9
 let n = 0
@@ -26,47 +26,7 @@ function getNextTableElement(numberOfRow, now) {
     }   
 }
 
-function addNewSystem(botton) {
 
-
-
-
-    let system = document.createElement('tr');
-    system.id = `row${n}`;
-    
-    system.innerHTML = "<tr id=`row${n}`></tr>";
-    document.querySelector("#placeForNewSystem").append(system);
-    rowId = document.querySelector(`#row${n}`);
-
-
-    for (let i = 0; i < 6; i++) {
-        let contentOfSystem = ""
-        let now = new Date();
-        contentOfSystem = getNextTableElement(i, now);
-
-        rowId.insertAdjacentHTML('beforeend', contentOfSystem);
-    }
-
-    n += 1; 
-    let AlertAboutNoActiveSystem = document.querySelector("#noActiveSystem");
-
-    if (n > 0) {
-
-        AlertAboutNoActiveSystem.style.display = "none";        
-
-    }
-    if (n >= limitOfSystem) {
-        botton.style.display = "none";
-        
-        let limit = document.createElement('span');
-        limit.id = 'AlertAboutLimit';
-        limit.textContent = 'Достигнут лимит систем';
-        botton.after(limit);
-        
-        return 
-    } 
-
-}
 
 
 function deleteSystem(obj) {
