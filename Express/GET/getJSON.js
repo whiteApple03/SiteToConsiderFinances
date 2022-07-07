538f1bb012f6dc406e22ed7f5c2055d659ba0d6f
whiteApple03 <108337603+whiteApple03@users.noreply.github.com>
2022-07-07 10:50:41 +0700

    include ejs

---
+++ Express/GET/getJSON.js
@@ -0,0 +1,4 @@
+import  express  from "express";
+const add = express()
+
+add.get()
--- javascript/script.js
+++ javascript/script.js
@@ -1,24 +1,37 @@
-import  express  from 'express';
-import { appendFile } from 'fs';
+import { assert } from 'console';
+import  express, { json }  from 'express';
 import path from 'path';
 
-import { app } from '../Express/StartingApp/initializationApp.js'
-
+import { app } from '../Express/StartingApp/initializationApp.js';
+import GlobalVars from '../Vars/GlobalVars.json' assert {type: "json"};
+console.log();
 // import '../Buttons/EventsOfButton/addNewSystem.js';
 
-
 const __dirname = path.resolve();
+const PORT = process.env.PORT ?? 3000;
+
+
+
 
 app.use(express.static(path.resolve(__dirname, 'PagesHTML')));
+app.set('view engine', 'ejs');
 
-app.listen(3000, (err) => {
+
+
+app.get('/statistick', (req, res) => {
+    res.send(`${JSON.stringify(GlobalVars)}`)
+    res.sendFile(__dirname, 'Vars/GlobalVars.json')
+});
+
+app.listen(PORT, (err) => {
     if (err) throw err;
     console.log('Server is OK');
 });
 
-// add.get('/', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'PagesHTML', 'index.html'));
-// });
+
+
+
+
 
 
 
